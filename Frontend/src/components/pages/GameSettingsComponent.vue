3c1192c0a846d86df2f06fdc3ef58a5c85f39f57
SK <sufyaan.khan@hva.nl>
2024-03-18 15:23:29 +0100

    Added the start game button on my page, when you click on the button it takes you to the Game Page. And removed duplicate css code

--- Frontend/src/components/pages/GameSettingsComponent.vue
+++ Frontend/src/components/pages/GameSettingsComponent.vue
@@ -5,15 +5,16 @@
         <h2 class="header-title">Players ({{ totalPlayers }})</h2>
       </div>
       <div class="left-column-players-in-lobby">
-        <div v-for="(player, index) in players" :key="index" class="player-pill">
+        <div v-for="(player, index) in players" :key="index" class="player-pill transition">
           <p class="player-name">{{ player.name }} <span v-if="player.host">(Host)</span></p>
           <div class="player-status">
-            <button class="ready-button">Ready</button>
-            <button v-if="!player.host" class="left-column-kick-button" @click="kickPlayer(index)">Kick</button>
+            <button class="ready-button transition">Ready</button>
+            <button v-if="!player.host" class="kick-button transition" @click="kickPlayer(index)">Kick
+            </button>
           </div>
         </div>
-        <div v-if="canAddBot" class="player-pill">
-          <button class="left-column-add-button" @click="addBot">Add Bot</button>
+        <div v-if="canAddBot" class="player-pill transition">
+          <button class="add-bot-button transition" @click="addBot">Add Bot</button>
         </div>
       </div>
     </div>
@@ -24,17 +25,20 @@
       <div>
         <h3>Number of Players</h3>
         <span>{{ numberOfPlayers }}</span>
-        <input type="range" min="2" max="4" v-model="numberOfPlayers" class="center-column-slider">
+        <input type="range" min="2" max="4" v-model="numberOfPlayers" class="transition center-column-slider">
       </div>
       <div>
         <h3>Turn Duration (In minutes)</h3>
         <span>{{ turnDuration }}</span>
-        <input type="range" min="1" max="3" v-model="turnDuration" class="center-column-slider">
+        <input type="range" min="1" max="3" v-model="turnDuration" class="transition center-column-slider">
       </div>
       <div>
         <h3>Points to win</h3>
         <span>{{ pointsToWin }}</span>
-        <input type="range" min="8" max="10" v-model="pointsToWin" class="center-column-slider">
+        <input type="range" min="8" max="10" v-model="pointsToWin" class="transition center-column-slider">
+      </div>
+      <div class="start-game-div">
+       <router-link to="/Game"><button class="start-game-button transition">Start Game</button></router-link>
       </div>
     </div>
     <div class="right-column-gamesettings-page">
@@ -78,24 +82,35 @@ export default {
           "Gon", "Killua", "Saitama", "Mikasa", "Kurama"];
         const randomIndex = Math.floor(Math.random() * randomNames.length);
         const botName = randomNames[randomIndex] + " (Bot)"; // add "(bot)" to the random name
-        this.players.push({ name: botName });
+        this.players.push({name: botName});
       }
     },
     kickPlayer(index) {
-        if (!this.players[index].host) {
-          this.players.splice(index, 1);
-          if (this.players.every(player => !player.host)) {
-            this.botCount--;
-          } else {
-            this.botCount--;
-          }
+      if (!this.players[index].host) {
+        this.players.splice(index, 1);
+        if (this.players.every(player => !player.host)) {
+          this.botCount--;
+        } else {
+          this.botCount--;
         }
       }
     }
   }
+}
 </script>
 
 <style scoped>
+.transition {
+  transition: all 0.4s ease;
+}
+
+.transition:hover {
+  transition: all 0.4s ease;
+  top: 10%;
+  transform: translateY(-10%);
+  border: none;
+}
+
 .container {
   display: flex;
 }
@@ -124,68 +139,9 @@ export default {
   font-weight: bold;
 }
 
-.ready-button {
-  color: white;
-  background-color: #1FAB1C;
-  font-size: 20px;
-  font-weight: bold;
-  transition: all 0.4s ease;
-  border: none;
-  width: 125px;
-  height: 30px;
-  border-radius: 25px;
-}
-
-.ready-button:hover {
-  color: #1FAB1C;
-  background-color: white;
-  transition: all 0.4s ease;
-  top: 10%;
-  transform: translateY(-10%);
-  border: none;
-}
-
-.left-column-kick-button {
-  color: white;
-  background-color: #FC1212;
-  font-size: 20px;
-  font-weight: bold;
-  transition: all 0.4s ease;
-  border: none;
-  width: 125px;
-  height: 30px;
-  border-radius: 25px;
-}
-
-.left-column-kick-button:hover {
-  color: #FC1212;
-  background-color: white;
-  transition: all 0.4s ease;
-  top: 10%;
-  transform: translateY(-10%);
-  border: none;
-}
-
-.left-column-add-button {
-  color: white;
-  background-color: #FFB54A;
-  font-size: 20px;
-  font-weight: bold;
-  transition: all 0.4s ease;
-  border: none;
-  width: 125px;
-  height: 30px;
-  margin: auto;
-  border-radius: 25px;
-}
-
-.left-column-add-button:hover {
-  color: #FFB54A;
-  background-color: white;
-  transition: all 0.4s ease;
-  top: 10%;
-  transform: translateY(-10%);
-  border: none;
+.player-status {
+  display: flex;
+  gap: 0.5rem;
 }
 
 .player-pill {
@@ -198,20 +154,84 @@ export default {
   margin: 1rem;
   width: 400px;
   height: 50px;
-  transition: all 0.4s ease;
 }
 
 .player-pill:hover {
   background-color: black;
   color: white;
-  transition: all 0.4s ease;
-  top: 10%;
-  transform: translateY(-10%);
 }
 
-.player-status {
+.ready-button {
+  color: white;
+  background-color: #1FAB1C;
+  font-size: 20px;
+  font-weight: bold;
+  border: none;
+  width: 125px;
+  height: 30px;
+  border-radius: 25px;
+}
+
+.ready-button:hover {
+  color: #1FAB1C;
+  background-color: white;
+}
+
+.start-game-button {
+  color: white;
+  background-color: #60BFB2;
+  font-size: 20px;
+  font-weight: bold;
+  border: none;
+  width: 200px;
+  height: 40px;
+  border-radius: 10px;
+  margin: auto;
+  text-decoration: none;
+}
+
+.start-game-button:hover {
+  color: #60BFB2;
+  background-color: white;
+}
+
+.start-game-div {
   display: flex;
-  gap: 0.5rem;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.kick-button {
+  color: white;
+  background-color: #FC1212;
+  font-size: 20px;
+  font-weight: bold;
+  border: none;
+  width: 125px;
+  height: 30px;
+  border-radius: 25px;
+}
+
+.kick-button:hover {
+  color: #FC1212;
+  background-color: white;
+}
+
+.add-bot-button {
+  color: white;
+  background-color: #FFB54A;
+  font-size: 20px;
+  font-weight: bold;
+  border: none;
+  width: 125px;
+  height: 30px;
+  margin: auto;
+  border-radius: 25px;
+}
+
+.add-bot-button:hover {
+  color: #FFB54A;
+  background-color: white;
 }
 
 .center-column-slider {
@@ -223,7 +243,6 @@ export default {
   outline: none;
   opacity: 0.7;
   -webkit-transition: .2s;
-  transition: all 0.4s ease;;
 }
 
 .center-column-slider::-webkit-slider-thumb {
@@ -249,27 +268,18 @@ export default {
 .center-column-slider:hover {
   background: #60BFB2;
   height: 17px;
-  transition: all 0.4s ease;
-  top: 10%;
-  transform: translateY(-10%);
 }
 
 .center-column-slider::-webkit-slider-thumb:hover {
   background: #FF7B4D;
   width: 30px;
   height: 30px;
-  transition: all 0.4s ease;
-  top: 10%;
-  transform: translateY(-10%);
 }
 
 .center-column-slider::-moz-range-thumb:hover {
   background: #FF7B4D;
   width: 30px;
   height: 30px;
-  transition: all 0.4s ease;
-  top: 10%;
-  transform: translateY(-10%);
 }
 
 </style>
