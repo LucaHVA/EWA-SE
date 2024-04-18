9f946350213768638fa54932c4bf5efbf20d2d3d
rijkbol <rijkbol@ad.hva.nl>
2024-04-18 14:03:25 +0200

    Made socket package and class

---
+++ backend/src/main/java/org/example/backend/sockets/NotificationDistributor.java
@@ -0,0 +1,7 @@
+package org.example.backend.sockets;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+@Component
+public class NotificationDistributor extends TextWebSocketHandler {
+}
