52b7ab3d488356be50cb60ef2a805fbd2138085b
Stephanie <langatje@gmail.com>
2024-04-23 15:24:27 +0200

    css folder change (from main.js)

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,6 +1,7 @@
 import {createApp} from 'vue'
 import {router} from "@/router";
 import AppComponent from './App.vue'
+import './assets/css/main.css'
 
 
 createApp(AppComponent).use(router).mount('#app')
