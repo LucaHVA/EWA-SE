0c665ec7574bc3a4b8807d0d35a7376558a20959
SK <sufyaan.khan@hva.nl>
2024-05-28 14:40:00 +0200

    Very very cool navbar :)

--- Frontend/src/components/NavBarComponent.vue
+++ Frontend/src/components/NavBarComponent.vue
@@ -2,35 +2,25 @@
   <nav class="navigation">
     <div class="navbar">
       <ul class="navbar-list">
-        <li>
-          <ul>
-            <li class="nav-item">
-              <router-link to="/home" class="nav-link" active-class="active">Home</router-link>
-            </li>
-            <!--            <li class="nav-item">-->
-            <!--              <router-link to="/leaderboard" class="nav-link" active-class="active">Leaderboard</router-link>-->
-            <!--            </li>-->
-            <!-- <li class="nav-item">-->
-            <!--    <router-link to="/tutorial" class="nav-link" active-class="active">Tutorial</router-link>-->
-            <!-- </li>-->
-          </ul>
+        <li class="nav-item">
+          <router-link to="/home" class="nav-link" active-class="active">Home</router-link>
         </li>
-
-        <li>
-          <ul class="user-logging">
-            <li class="nav-item" v-if="isLoggedIn">
-              <router-link to="/profilePage" class="nav-link" active-class="active">Profile</router-link>
-            </li>
-            <li class="nav-item" v-else>
-              <router-link to="/login" class="nav-link" active-class="active">Log in</router-link>
-            </li>
-            <li class="nav-item" v-if="isLoggedIn">
-              <router-link to="/home" @click.prevent="logout" class="nav-link" active-class="active">Logout</router-link>
-            </li>
-            <li class="nav-item" v-else>
-              <router-link to="/register" class="nav-link" active-class="active">Register</router-link>
-            </li>
-          </ul>
+        <li class="nav-item" v-if="isLoggedIn">
+          <router-link to="/leaderboard" class="nav-link" active-class="active">Leaderboard</router-link>
+        </li>
+      </ul>
+      <ul class="user-logging">
+        <li class="nav-item" v-if="isLoggedIn">
+          <router-link to="/profilePage" class="nav-link" active-class="active">Profile</router-link>
+        </li>
+        <li class="nav-item" v-else>
+          <router-link to="/login" class="nav-link" active-class="active">Log in</router-link>
+        </li>
+        <li class="nav-item" v-if="isLoggedIn">
+          <a href="#" @click="logout" class="nav-link">Logout</a>
+        </li>
+        <li class="nav-item" v-else>
+          <router-link to="/register" class="nav-link" active-class="active">Register</router-link>
         </li>
       </ul>
     </div>
@@ -41,18 +31,19 @@
 export default {
   name: "NavbarComponent",
 
-  inject:["usersService"],
+  inject: ["usersService"],
   computed: {
     isLoggedIn() {
       // Check if user information exists in local storage
-      return localStorage.getItem('userInfo') !== null;
+      return sessionStorage.getItem('undefined_ACC') !== null;
     }
   },
   methods: {
-   async logout() {
+    async logout() {
       // Clear user info from local storage
-      localStorage.removeItem('userInfo');
+      sessionStorage.removeItem('undefined_ACC');
       await this.usersService.signOut()
+      this.$router.push('/home');
       window.location.reload();
     }
   }
@@ -67,53 +58,66 @@ nav {
   font-family: "Raleway", sans-serif;
   font-weight: 600;
   font-size: 20px;
-
 }
 
 nav ul {
   display: flex;
   list-style-type: none;
-}
-
-/* Logo */
-#left-logo {
-  margin: 20px;
-}
-
-.navbar_logo {
-  width: 15%;
+  padding: 0;
+  margin: 0;
 }
 
 /* Navigation */
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+}
+
 .navbar-list {
   display: flex;
   justify-content: center;
+  flex-grow: 1;
 }
 
 .nav-link {
+  font-weight: bold;
+  transition: all 0.4s ease;
   text-align: center;
   text-decoration: none;
   cursor: pointer;
   color: black;
 }
 
-/*.nav-link:hover::after {*/
-/*  content: '';*/
-/*  position: absolute;*/
-/*  bottom: 0;*/
-/*  left: 0;*/
-/*  width: 100%;*/
-/*  height: 2px;*/
-/*  background-color: black; !* Add black line under link on hover *!*/
-/*}*/
+.nav-link:hover {
+  color: var(--coral);
+}
 
 .nav-item {
+  transition: all 0.5s ease;
   margin: 0 10px;
 }
 
-/* User logging */
-.user-logging {
-  justify-content: flex-end;
+.nav-item:hover {
+  transition: all 0.5s ease;
+  top: 10%;
+  transform: translateY(-25%);
+  padding: 5px 10px;
+  border-bottom: 1px solid var(--coral);
 }
 
+.active {
+  font-weight: bold;
+  color: var(--coral);
+  border-bottom: 1px solid var(--coral);
+}
+
+
+/* User logging */
+.user-logging {
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 5rem;
+}
 </style>
